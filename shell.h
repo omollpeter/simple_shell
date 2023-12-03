f01ad6db44d22d0f1095868f0aed5333c2bb9ebc
Peter Omollo <omollpeter@gmail.com>
2023-12-03 23:26:58 +0300

    Create header file and create files for functions and main program

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+
+#endif /* SIMPLE_SHELL_H */
